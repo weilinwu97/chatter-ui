69b15dd2f2f57ef0c7dd0e681788e91009ebc558
Weilin (Alan) Wu <wwu@ccia.org.au>
2026-01-16 22:30:45 +1100

    set up login and signup page, set up apollo client to send requests to graphql backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import { createTheme, ThemeProvider, CssBaseline, Container } from '@mui/material'
-import { RouterProvider } from 'react-router-dom'
-import router from './components/Routes'
+import { createTheme, ThemeProvider, CssBaseline, Container } from '@mui/material';
+import { RouterProvider } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client/react';
+import client from "./constants/apollo-client"
+import router from './components/Routes';
 
 const darkTheme = createTheme({
   palette: {
     mode: 'dark'
   }
-})
+});
 
 const App = () => {
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Container>
-        <RouterProvider router={router} />
-      </Container>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Container>
+          <RouterProvider router={router} />
+        </Container>
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
